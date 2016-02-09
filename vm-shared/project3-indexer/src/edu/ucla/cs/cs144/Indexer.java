d8c654e0249bf65a8846dc4ddd50c5266824a98e
Daniel Geng <danielgeng@gmail.com>
2016-02-08 16:45:16 -0800

    update indexer with lucene notes

--- vm-shared/project3-indexer/src/edu/ucla/cs/cs144/Indexer.java
+++ vm-shared/project3-indexer/src/edu/ucla/cs/cs144/Indexer.java
@@ -23,9 +23,35 @@ import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
 public class Indexer {
+    public final static String LUCENE_DIR = "/var/lib/lucene";
     
     /** Creates a new instance of Indexer */
     public Indexer() {
+    }
+
+    private IndexWriter indexWriter = null;
+
+    public IndexWriter getIndexWriter(boolean create) throws IOException {
+        if (indexWriter == null) {
+            try {
+                Directory indexDir = FSDirectory.open(new File(LUCENE_DIR + "/index1"));
+                IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_2, new StandardAnalyzer());
+                indexWriter = new IndexWriter(indexDir, config);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return indexWriter;
+    }
+
+    public void closeIndexWriter() throws IOException {
+        if (indexWriter != null) {
+            try {
+                indexWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
  
     public void rebuildIndexes() {
@@ -33,39 +59,48 @@ public class Indexer {
         Connection conn = null;
 
         // create a connection to the database to retrieve Items from MySQL
-	try {
-	    conn = DbManager.getConnection(true);
-	} catch (SQLException ex) {
-	    System.out.println(ex);
-	}
+    	try {
+    	    conn = DbManager.getConnection(true);
+    	} catch (SQLException e) {
+            e.printStackTrace();
+    	}
 
+        try {
+            getIndexWriter(true);
 
-	/*
-	 * Add your code here to retrieve Items using the connection
-	 * and add corresponding entries to your Lucene inverted indexes.
-         *
-         * You will have to use JDBC API to retrieve MySQL data from Java.
-         * Read our tutorial on JDBC if you do not know how to use JDBC.
-         *
-         * You will also have to use Lucene IndexWriter and Document
-         * classes to create an index and populate it with Items data.
-         * Read our tutorial on Lucene as well if you don't know how.
-         *
-         * As part of this development, you may want to add 
-         * new methods and create additional Java classes. 
-         * If you create new classes, make sure that
-         * the classes become part of "edu.ucla.cs.cs144" package
-         * and place your class source files at src/edu/ucla/cs/cs144/.
-	 * 
-	 */
+            /*
+             * Add your code here to retrieve Items using the connection
+             * and add corresponding entries to your Lucene inverted indexes.
+                 *
+                 * You will have to use JDBC API to retrieve MySQL data from Java.
+                 * Read our tutorial on JDBC if you do not know how to use JDBC.
+                 *
+                 * You will also have to use Lucene IndexWriter and Document
+                 * classes to create an index and populate it with Items data.
+                 * Read our tutorial on Lucene as well if you don't know how.
+                 *
+                 * As part of this development, you may want to add 
+                 * new methods and create additional Java classes. 
+                 * If you create new classes, make sure that
+                 * the classes become part of "edu.ucla.cs.cs144" package
+                 * and place your class source files at src/edu/ucla/cs/cs144/.
+             * 
+             */ 
+
+            closeIndexWriter();
+            conn.close();
+
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
 
 
         // close the database connection
-	try {
-	    conn.close();
-	} catch (SQLException ex) {
-	    System.out.println(ex);
-	}
+    	try {
+    	    conn.close();
+    	} catch (SQLException e) {
+    	    e.printStackTrace();
+    	}
     }    
 
     public static void main(String args[]) {
